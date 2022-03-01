bfd455d8787b58187e5a948fe1e644d9d0f159e8
irfanizudin <irfanizzudyn@gmail.com>
2022-03-01 21:44:34 +0700

    Setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#1983FF",
+        gray: "#F1F1F1",
+      },
+    },
+  },
+  plugins: [],
+};
